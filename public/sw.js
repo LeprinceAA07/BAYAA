b5f1d5a31b9ff671c9fbf270a299ab0b9133e98f
LeprinceAA07 <medsalemlestar07@gmail.com>
2026-09-12 05:55:22 +0000

    Add BAYAA PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+const CACHE='bayaa-v1';
+const APP_SHELL=['/','/manifest.webmanifest','/icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response}).catch(()=>cached))) });
